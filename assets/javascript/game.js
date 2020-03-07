615c7a1c7c388e16cc583c745eab4e5308908311
msyphus <mark.syphus@gmail.com>
2020-03-06 23:54:25 -0700

    added functionality to New Game button to pick a random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,19 @@ var rubs = 5;  //number of belly rubs left
 var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
-var word = words[Math.floor(Math.random() * words.length)]; //the randomly picked word to guess
 var blanks = [];
 var choices = []; //letters the user has guessed
 
-//Display word in blanks
-for (var i =0; i < word.length; i++) {
-    blanks[i] = "_";
+//generate a random word
+function randWord() {
+    var word = words[Math.floor(Math.random() * words.length)];
+    for (var i =0; i < word.length; i++) {
+        blanks[i] = "_";
+    }
+    document.getElementById("blanks").innerHTML = word;
 }
+
+//Display word in blanks
 
 
 
