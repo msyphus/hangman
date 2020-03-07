dddc97b7c227e45a322f39efde640602065a9af1
msyphus <mark.syphus@gmail.com>
2020-03-06 23:39:57 -0700

    added New Game button, created list of variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,19 @@
-var huhu = ["../Images/belly01", "../Images/warn02", "../Images/bite02"]
-var escapes = 0;
-var bites = 0
+// Variables
+var huhu = ["../Images/belly01", "../Images/warn02", "../Images/bite02"]; //Huhu's images
+var escapes = 0; //number of wins
+var bites = 0;  //number of loses
+var rubs = 5;  //number of belly rubs left
+var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
+var word = words[Math.floor(Math.random() * words.length)]; //the randomly picked word to guess
+var blanks = [];
+var choices = []; //letters the user has guessed
+
+//Display word in blanks
+for (var i =0; i < word.length; i++) {
+    blanks[i] = "_";
+}
+
 
 
