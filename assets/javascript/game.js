c2694b8bf3b5405f38ac66991486f578a30fff1d
msyphus <mark.syphus@gmail.com>
2020-03-07 12:23:51 -0700

    display random word as blanks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,19 +6,14 @@ var rubs = 5;  //number of belly rubs left
 var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
-var blanks = [];
 var choices = []; //letters the user has guessed
 
 //generate a random word
 function randWord() {
     var word = words[Math.floor(Math.random() * words.length)];
-    for (var i =0; i < word.length; i++) {
-        blanks[i] = "_";
+    var blanks = [];
+    for (var i = 0; i < word.length; i++) {
+    blanks[i] = "_";
     }
-    document.getElementById("blanks").innerHTML = word;
+    document.getElementById("blanks").innerHTML = blanks.join(" ");
 }
-
-//Display word in blanks
-
-
-
