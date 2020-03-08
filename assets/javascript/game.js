961e8e8966c6624d62447afe0ffd92866a3ea97f
msyphus <mark.syphus@gmail.com>
2020-03-07 21:22:16 -0700

    added belly rubs left count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,18 @@ var huhu = ["../Images/belly01", "../Images/warn02", "../Images/bite02"]; //Huhu
 var escapes = 0; //number of wins
 var bites = 0;  //number of loses
 var rubs = 5;  //number of belly rubs left
+var correctGuess;  //count the correct guesses
+var spaces; //number of spaces in the word
 var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
-var choices = []; //letters the user has guessed
+var lettersGuessed = []; //letters the user has guessed
+var currIndex;  //index of the current word
+var wordGuesses = []; //the word being built to match the hidden word
+var gameStart = false;
+var gameEnd = false;
 
-//generate a random word
+//generate a random word and display as blanks
 function randWord() {
     var word = words[Math.floor(Math.random() * words.length)];
     var blanks = [];
@@ -16,4 +22,11 @@ function randWord() {
     blanks[i] = "_";
     }
     document.getElementById("blanks").innerHTML = blanks.join(" ");
+    console.log(word)
 }
+
+document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs;
+
+
+
+
