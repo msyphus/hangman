1e0b6876261ca1701f9c0bcd9c22b506b99d0080
msyphus <mark.syphus@gmail.com>
2020-03-07 21:53:16 -0700

    placed letters depending on correct or not, reduced rubs for incorrect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ var spaces; //number of spaces in the word
 var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
+var word = words[Math.floor(Math.random() * words.length)];
 var lettersGuessed = []; //letters the user has guessed
 var currIndex;  //index of the current word
 var wordGuesses = []; //the word being built to match the hidden word
@@ -16,17 +17,27 @@ var gameEnd = false;
 
 //generate a random word and display as blanks
 function randWord() {
-    var word = words[Math.floor(Math.random() * words.length)];
     var blanks = [];
     for (var i = 0; i < word.length; i++) {
     blanks[i] = "_";
     }
     document.getElementById("blanks").innerHTML = blanks.join(" ");
-    console.log(word)
 }
 
-document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs;
+document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs; 
 
+document.onkeyup = function(event) {
+    var userChoice = event.key;
+if (word.includes(event.key)) {
+    var correct = document.getElementById("answer");
+    correct.textContent = event.key;
+} else {
+    var incorrect = document.getElementById("letterList-text");
+    incorrect.textContent = event.key;
+    rubs--;
+    document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs;
+}
+}
 
 
 
