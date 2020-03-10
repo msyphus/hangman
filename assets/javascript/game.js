686159dc29439df99c668ec8bb77a3e4f555045d
msyphus <mark.syphus@gmail.com>
2020-03-09 21:15:38 -0600

    change image and caption based on rub count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // Variables
-var huhu = ["../Images/belly01", "../Images/warn02", "../Images/bite02"]; //Huhu's images
+//var huhu = ["../Images/belly01", "../Images/warn02", "../Images/bite02"]; //Huhu's images
 var escapes = 0; //number of wins
 var bites = 0;  //number of loses
 var rubs = 5;  //number of belly rubs left
-var correctGuess;  //count the correct guesses
-var spaces; //number of spaces in the word
+//var correctGuess;  //count the correct guesses
+//var spaces; //number of spaces in the word
 var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
@@ -20,8 +20,9 @@ function randWord() {
     for (var i = 0; i < word.length; i++) {
     wordGuesses[i] = "_";
     }
-    document.getElementById("answer").innerHTML = wordGuesses.join(" ");
-}
+    document.getElementById("answer").innerHTML = wordGuesses.join(" ");     
+};
+
 
 document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs; 
 
@@ -36,16 +37,25 @@ document.onkeyup = function(event) {
         }
         document.getElementById("answer").textContent = wordGuesses.join(" ");
     } else {
-        if (rubs > 0) {
+        if (rubs >= 1) {
         var incorrect = document.getElementById("letterList-text");
         incorrect.textContent = userChoice;
         rubs--;
         document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs;
+        if (rubs >=3) {
+            document.getElementById("huImg").src = "Assets/Images/belly01.jpg";
+            document.getElementById("caption").innerHTML = "What a sweet boy!";
         } else {
+            document.getElementById("huImg").src = "Assets/Images/warn02.jpg";
+            document.getElementById("caption").innerHTML = "I don't like the look on his face!";
+        }
+        } else {
+            document.getElementById("huImg").src = "Assets/Images/bite02.jpg";
+            document.getElementById("caption").innerHTML = "Woohoo!  Free acupuncture!";
             document.getElementById("answer").innerHTML = "Ouch! He caught you!";
         }
     }
-}
+};
 
 
 
