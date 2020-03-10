ffe1346bf9f0ba12ea05c3df94aed2b228383368
msyphus <mark.syphus@gmail.com>
2020-03-09 23:15:26 -0600

    created array for storing incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,16 @@ var bites = 0;  //number of loses
 var rubs = 5;  //number of belly rubs left
 //var correctGuess;  //count the correct guesses
 //var spaces; //number of spaces in the word
-var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
+//var status = ["What a sweet boy!", "I don't like the look on his face!", "Woohoo!  Free acupuncture!"] ;  //the image captions
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var words = ["fuzz", "bengal", "sokoke", "fiesty", "playful", "purrfect", "buddy", "fur ball"];
 var word = words[Math.floor(Math.random() * words.length)];
-var lettersGuessed = []; //letters the user has guessed
+//var lettersGuessed = []; //letters the user has guessed
 var currIndex;  //index of the current word
 var wordGuesses = []; //the word being built to match the hidden word
-var gameStart = false;
-var gameEnd = false;
+var wrong = [];
+//var gameStart = false;
+//var gameEnd = false;
 
 //generate a random word and display as blanks
 function randWord() {
@@ -25,21 +26,25 @@ function randWord() {
 
 
 document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs; 
+document.getElementById("wins").textContent = "You've escaped " + escapes + " bites";
+document.getElementById("losses").textContent = "Youv'e been bitten " + bites + "times";
 
 document.onkeyup = function(event) {
     
     var userChoice = event.key;
-    currIndex = word.indexOf(userChoice, 0);
-    if (currIndex >= 0 && rubs > 0) {
-        while (currIndex >=0) {
-            wordGuesses[currIndex] = userChoice;
-            currIndex=word.indexOf(userChoice, currIndex + 1);
+    wordIndex = word.indexOf(userChoice, 0);
+    if (wordIndex >= 0 && rubs > 0) {
+        while (wordIndex >=0) {
+            wordGuesses[wordIndex] = userChoice;
+            wordIndex=word.indexOf(userChoice, wordIndex + 1);
         }
         document.getElementById("answer").textContent = wordGuesses.join(" ");
     } else {
-        if (rubs >= 1) {
+        if (rubs > 1) {
         var incorrect = document.getElementById("letterList-text");
-        incorrect.textContent = userChoice;
+        wrong.push(event.key);
+        console.log(wrong)
+        incorrect.textContent = wrong;
         rubs--;
         document.getElementById("rubsLeft").textContent = "Belly Rubs Left: " + rubs;
         if (rubs >=3) {
@@ -53,6 +58,7 @@ document.onkeyup = function(event) {
             document.getElementById("huImg").src = "Assets/Images/bite02.jpg";
             document.getElementById("caption").innerHTML = "Woohoo!  Free acupuncture!";
             document.getElementById("answer").innerHTML = "Ouch! He caught you!";
+            document.getElementById("rubsLeft").textContent = "Belly Rubs Left: Yikes!";
         }
     }
 };
